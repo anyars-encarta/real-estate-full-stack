70b1353f19278717d872e392192c418c2651d53a
anyars-encarta <anyarsencarta@gmail.com>
2024-04-05 21:27:39 +0000

    Add update profile functionality

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,58 @@
+import prisma from "../lib/prisma.js";
+
+export const getUsers = async (req, res) => {
+    console.log('It works')
+    try {
+        const users =  await prisma.user.findMany();
+        res.status(200).json(users)
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({ message: 'Failed to get users!'})
+    };
+};
+
+export const getUser = async (req, res) => {
+    const id = req.params.id
+    try {
+        const user =  await prisma.user.findUnique({
+            where: { id }
+        });
+        res.status(200).json(user)
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({ message: 'Failed to get user!'})
+    };
+};
+
+
+export const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const tokenUserId = req.userId;
+    const body = req.body;
+
+    if(id !== tokenUserId) {
+        return res.status(403).json({ message: 'Not Authorized!'})
+    }
+
+    try {
+        const updatedUser =  await prisma.user.update({
+            where: { id }, 
+            data: body,
+        });
+
+        res.status(200).json(updatedUser)
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({ message: 'Failed to update user!'})
+    };
+};
+
+
+export const deleteUser = async (req, res) => {
+    try {
+
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({ message: 'Failed to delete user!'})
+    };
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,9 +4,7 @@ import { login, logout, register } from '../controllers/auth.controller.js';
 const authRoute = express.Router();
 
 authRoute.post('/register', register);
-
 authRoute.post('/login', login);
-
 authRoute.post('/logout', logout);
 
 export default authRoute;
