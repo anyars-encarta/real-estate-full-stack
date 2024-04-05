b0585a755666dbb22ba95283afc600c49160d22b
anyars-encarta <anyarsencarta@gmail.com>
2024-04-05 23:31:28 +0000

    Complete Update user API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -29,7 +29,7 @@ export const getUser = async (req, res) => {
 export const updateUser = async (req, res) => {
     const id = req.params.id;
     const tokenUserId = req.userId;
-    const { password, ...inputs } = req.body;
+    const { password, avatar, ...inputs } = req.body;
 
     if(id !== tokenUserId) {
         return res.status(403).json({ message: 'Not Authorized!'})
@@ -46,6 +46,7 @@ export const updateUser = async (req, res) => {
             data: {
                 ...inputs,
                 ...(updatedPassword && { password: updatedPassword }),
+                ...(avatar && { avatar }),
             },
         });
 
@@ -58,8 +59,19 @@ export const updateUser = async (req, res) => {
 
 
 export const deleteUser = async (req, res) => {
-    try {
+    const id = req.params.id;
+    const tokenUserId = req.userId;
 
+    if(id !== tokenUserId) {
+        return res.status(403).json({ message: 'Not Authorized!'})
+    }
+
+    try {
+        await prisma.user.delete({
+            where: { id }
+        })
+
+        res.status(200).json({ message: 'User deleted successfully!'})
     } catch(e) {
         console.log(e)
         res.status(500).json({ message: 'Failed to delete user!'})
