1ba8f4c484e943ec40282482497bcd9db7e055be
anyars-encarta <anyarsencarta@gmail.com>
2024-04-06 02:41:37 +0000

    Correct react imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
 // import Navbar from './components/navbar/Navbar';
 import Home from './pages/homePage/Home';
