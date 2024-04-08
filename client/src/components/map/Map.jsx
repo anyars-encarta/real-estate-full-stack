e392d1bd59ad7a744c29f19ebe11123c4e403946
anyars-encarta <anyarsencarta@gmail.com>
2024-04-08 13:41:13 +0000

    Complete fetch single posts

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -3,10 +3,21 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 import './map.scss';
 import 'leaflet/dist/leaflet.css';
 import Pin from '../pin/Pin';
+import { useLoaderData } from 'react-router';
 
 const Map = ({ items }) => {
+    const post = useLoaderData();
+
     return (
-        <MapContainer center={[52.4797, -1.90269]} zoom={7} scrollWheelZoom={false} className='map'>
+        <MapContainer center={
+            items.length === 1
+                ? [items[0].latitude, items[0].longitude]
+                : [52.4797, -1.90269]
+        }
+            zoom={7}
+            scrollWheelZoom={false}
+            className='map'
+        >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
