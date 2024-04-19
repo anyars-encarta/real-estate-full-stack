98096dd7ba466289b00c9557b6d02dd0c9da5694
anyars-encarta <anyarsencarta@gmail.com>
2024-04-19 17:48:41 +0000

    Correct deployment URL

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
+    // baseURL: 'http://localhost:8800/api',
     baseURL: 'https://real-estate-api-coral.vercel.app/api', 
     withCredentials: true,
 });
