ec37dbac548bc71ceba3510cba635b2a29af66f6
anyars-encarta <anyarsencarta@gmail.com>
2024-04-20 09:19:00 +0000

    Update BaseURL in Client

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
     baseURL: 'http://localhost:8800/api',
-    // baseURL: 'https://real-estate-api-coral.vercel.app/api', 
+    // baseURL: 'https://real-estate-api-anyars.vercel.app/api', 
     withCredentials: true,
 });
 
