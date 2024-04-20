d306139776b4893143fe6a94221c11ff0c67f9ad
anyars-encarta <anyarsencarta@gmail.com>
2024-04-20 11:03:46 +0000

    Correct app listening port

--- api/index.js
+++ api/index.js
@@ -31,6 +31,4 @@ app.get('/api', (req, res) => {
     res.send(`✅ Server is running on port ${PORT}!`);
 })
 
-app.listen(PORT, () => {
-    console.log(`✅ Server is running on port ${PORT}! Hurrah!!`);
-})
+app.listen(PORT, () => console.log(`✅ Server is running on port ${PORT}! Hurrah!!`));
